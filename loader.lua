bcde86b51befe663c085a93bbc184c6b9e08e55b
reighnormanblessfrancist-boop <reighnormanblessfrancist@gmail.com>
2025-09-06 17:29:45 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,29 @@
 -- Super Hub Loader
-local url = "https://raw.githubusercontent.com/reighnormanblessfrancist-boop/SuperHub/main/hub.lua"
-loadstring(game:HttpGet(url))()
+pcall(function()
+    local url = "https://raw.githubusercontent.com/reighnormanblessfrancist-boop/SuperHub/main/SuperHub.lua"
+    local code = game:HttpGet(url)
+    loadstring(code)()
+
+    -- Successful load notification
+    if Rayfield then
+        Rayfield:Notify({
+            Title = "Super Hub by supan",
+            Content = "thank u for choosing this rayfield script",
+            Duration = 10,
+            Image = "thanks"
+        })
+    end
+end)
+
+-- Failed load notification
+pcall(function()
+    local success = pcall(function() game:HttpGet("https://raw.githubusercontent.com/reighnormanblessfrancist-boop/SuperHub/main/SuperHub.lua") end)
+    if not success and Rayfield then
+        Rayfield:Notify({
+            Title = "Super Hub by supan",
+            Content = "mail: you freaking failer",
+            Duration = 5,
+            Image = "bruh"
+        })
+    end
+end)
